cee68c3910c6dacfc8ecd934221957b032afaa12
Lucas Sadoulet <lls5617@psu.edu>
2025-04-08 18:41:01 -0400

    cleaning trading

--- trading.py
+++ trading.py
@@ -12,26 +12,31 @@ class Trader:
     def run(self, state: TradingState):
         result = {}
         for product in state.order_depths:
-            if(product!="RAINFOREST_RESIN"):
-                continue
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            buy_price = 9998 
-            sell_price = 10002
-    
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < buy_price:
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > sell_price:
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+            if(product=="RAINFOREST_RESIN"):
+
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: List[Order] = []
+                buy_price = 9998 
+                sell_price = 10002
+        
+                if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    if int(best_ask) < buy_price:
+                        orders.append(Order(product, best_ask, -best_ask_amount))
+        
+                if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    if int(best_bid) > sell_price:
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+                
+                result[product] = orders
+                print(jsonpickle.encode(state.observations))
+        
+            if(product=="SQUID_INK"):
+                pass
             
-            result[product] = orders
-            print(jsonpickle.encode(state.observations))
-    
+            if(product=="KELP"):
+                pass
         traderData = "SAMPLE"
 
         conversions = 1
